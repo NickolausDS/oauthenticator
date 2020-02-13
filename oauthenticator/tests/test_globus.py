79fd6ff0f7140131136f2dad800a9fa390c5849b
Nickolaus D. Saint <nick@globus.org>
2020-02-13 11:08:24 -0800

    Rewrote Globus tests to use generics

--- oauthenticator/tests/test_globus.py
+++ oauthenticator/tests/test_globus.py
@@ -1,64 +1,122 @@
-from pytest import fixture, mark, raises
-from tornado import web, gen
+from io import BytesIO
+import json
+from base64 import b64decode
+from pytest import fixture, raises
+from tornado import web
+from tornado.httpclient import HTTPResponse
+
 from unittest.mock import Mock
 
-from globus_sdk import ConfidentialAppAuthClient
 
 from ..globus import GlobusOAuthenticator, GlobusLogoutHandler
 
-from .mocks import setup_oauth_mock, no_code_test, mock_handler
+from .mocks import setup_oauth_mock, mock_handler
 
 
 def user_model(username):
     """Return a user model"""
     return {
-        'login': username,
+        'preferred_username': username,
     }
 
 
-@fixture
-def mock_globus_sdk(monkeypatch):
-    """Mock the globus_sdk request for 'oauth2_exchange_code_for_tokens', and
-    mock some of the items within the returned 'Tokens' class. """
-
-    class Tokens:
-
-        by_resource_server = {
-            'transfer.api.globus.org': {'access_token': 'TRANSFER_TOKEN'},
-            'auth.globus.org': {'access_token': 'AUTH_TOKEN'}
-
-        }
-        id_token = {'preferred_username': 'wash@globusid.org'}
-
-        def decode_id_token(self, client):
-            return self.id_token
-
-    tokens = Tokens()
-    monkeypatch.setattr(
-        ConfidentialAppAuthClient,
-        'oauth2_exchange_code_for_tokens',
-        lambda self, code: tokens
-    )
-    return tokens
+def revoke_token_request_handler(request):
+    assert request.method == 'POST', request.method
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        header = auth_header.split(None, 1)[1]
+        print(header)
+        print(b64decode(header))
+        resp = BytesIO(json.dumps({'active': False}).encode('utf8'))
+        return HTTPResponse(request=request, code=200, buffer=resp)
+    else:
+        return HTTPResponse(request=request, code=401)
 
 
 @fixture
-def globus_client(client):
-    setup_oauth_mock(
-        client,
-        host=['auth.globus.org'],
-        access_token_path='/v2/oauth2/token',
-        user_path='/userinfo',
-        token_type='bearer',
-    )
+def mock_globus_token_response():
+    return {
+        'access_token': 'de48bedc44b79937f7aa67',
+        'id_token': 'ClRha2UgbXkgbG92ZSwgdGFrZSBteSBsYW5kClRha2UgbWUgd2hlcmUgSS'
+                    'BjYW5ub3Qgc3RhbmQKSSBkb24ndCBjYXJlIGNhdXNlIEknbSBzdGlsbCBm'
+                    'cmVlCllvdSBjYW4ndCB0YWtlIHRoZSBza3kgZnJvbSBtZQpUYWtlIG1lIG'
+                    '91dCwgdG8gdGhlIGJsYWNrClRlbGwgJ2VtIEkgYWluJ3QgY29taW5nIGJh'
+                    'Y2sKQnVybiB0aGUgbGFuZCBhbmQgYm9pbCB0aGUgc2VhCllvdSBjYW4ndC'
+                    'B0YWtlIHRoZSBza3kgZnJvbSBtZQpUaGVyZSdzIG5vIHBsYWNlIEkgY2Fu'
+                    'IGJlClNpbmNlIEkgZm91bmQgc2VyZW5pdHkKWW91IGNhbid0IHRha2UgdG'
+                    'hlIHNreSBmcm9tIG1lCg==',
+        'expires_in': 172800,
+        'resource_server': 'auth.globus.org',
+        'token_type': 'Bearer',
+        'state': '5a5929fa3c0210042c2fbb455e1e39d0',
+        'other_tokens': [{
+            'access_token': 'fceb9836f9b6d1ae7d',
+            'expires_in': 172800,
+            'resource_server': 'transfer.api.globus.org',
+            'token_type': 'Bearer',
+            'state': '5a5929fa3c0210042c2fbb455e1e39d0',
+            'scope': 'urn:globus:auth:scope:transfer.api.globus.org:all'}],
+        'scope': 'profile openid'}
+
+
+@fixture
+def globus_tokens_by_resource_server(mock_globus_token_response):
+    token_attrs = ['expires_in', 'resource_server', 'scope',
+                   'token_type', 'refresh_token', 'access_token']
+    auth_token_dict = {attr_name: mock_globus_token_response.get(attr_name) for attr_name in
+                       token_attrs}
+    other_tokens = [{attr_name: token_dict.get(attr_name) for attr_name in token_attrs}
+                    for token_dict in mock_globus_token_response['other_tokens']]
+    tokens = other_tokens + [auth_token_dict]
+    return {token_dict['resource_server']: token_dict for token_dict in tokens}
+
+
+def set_extended_token_response(client, host, access_token_path, new_token_response):
+    """The default client fixture does a nice job of checking the access code
+    response while returning tokens in the oauth2 spec, but Globus returns
+    a bunch of other tokens, including an id_token. We want to make sure we
+    capture the full Globus token response. This will attach the dict
+    new_token_response to the built-in test response if it returns successfully"""
+    # Find the existing endpoint, function pair in client.hosts
+    url, func = next(filter(lambda host: host[0]==access_token_path,
+                            client.hosts[host]))
+    # Wrap the built-in token response with our custom response, but only if
+    # it returns successfully with an access token!
+    def custom_token_response(request):
+        response = func(request)
+        if response.get('access_token'):
+            # The original access_token is checked,
+            new_token_response['access_token'] = response['access_token']
+            return new_token_response
+        else:
+            return response
+
+    # Return all existing paths with the addition of our custom wrapped handler.
+    hosts = filter(lambda chost: chost[0] != access_token_path, client.hosts[host])
+    client.add_host(host, [(url, custom_token_response)] + list(hosts))
     return client
 
 
 @fixture
-def mock_globus_user(mock_globus_sdk):
+def globus_client(client, mock_globus_token_response):
+    setup_oauth_mock(
+        client,
+        host='auth.globus.org',
+        access_token_path='/v2/oauth2/token',
+        user_path='/v2/oauth2/userinfo',
+        token_type='bearer',
+        token_request_style='post',
+    )
+    set_extended_token_response(client, 'auth.globus.org', '/v2/oauth2/token',
+                                mock_globus_token_response)
+    return client
+
+
+@fixture
+def mock_globus_user(globus_tokens_by_resource_server):
     class User:
         name = 'Wash'
-        state = {'tokens': mock_globus_sdk.by_resource_server}
+        state = {'tokens': globus_tokens_by_resource_server}
 
         async def get_auth_state(self):
             return self.state
@@ -68,9 +126,9 @@ def mock_globus_user(mock_globus_sdk):
     return User()
 
 
-async def test_globus(globus_client, mock_globus_sdk):
+async def test_globus(globus_client):
     authenticator = GlobusOAuthenticator()
-    handler = globus_client.handler_for_user(user_model('wash'))
+    handler = globus_client.handler_for_user(user_model('wash@uflightacademy.edu'))
     data = await authenticator.authenticate(handler)
     assert data['name'] == 'wash'
     tokens = list(data['auth_state']['tokens'].keys())
@@ -85,69 +143,59 @@ async def test_globus_pre_spawn_start(mock_globus_user):
     assert 'GLOBUS_DATA' in spawner.environment
 
 
-async def test_allow_refresh_tokens(globus_client, mock_globus_sdk, monkeypatch):
+async def test_allow_refresh_tokens(globus_client, mock_globus_token_response):
+    """
+    Mock test setting GlobusOAuthenticator.allow_refresh_tokens = True
+    """
+    mock_globus_token_response['refresh_token'] = 'user_refresh_token'
+    set_extended_token_response(globus_client, 'auth.globus.org',
+                                '/v2/oauth2/token', mock_globus_token_response)
     authenticator = GlobusOAuthenticator()
-    # Sanity check, this field should be set to True
-    assert authenticator.allow_refresh_tokens is True
-    authenticator.allow_refresh_tokens = False
-    monkeypatch.setattr(
-        ConfidentialAppAuthClient,
-        'oauth2_start_flow',
-        Mock()
-    )
-    handler = globus_client.handler_for_user(user_model('wash'))
-    await authenticator.authenticate(handler)
-    ConfidentialAppAuthClient.oauth2_start_flow.assert_called_with(
-        authenticator.get_callback_url(None),
-        requested_scopes=' '.join(authenticator.scope),
-        refresh_tokens=False
-    )
+    handler = globus_client.handler_for_user(user_model('wash@uflightacademy.edu'))
+    response = await authenticator.authenticate(handler)
+    transfer_tokens = response['auth_state']['tokens']['transfer.api.globus.org']
+    assert 'refresh_token' in transfer_tokens
 
 
-async def test_restricted_domain(globus_client, mock_globus_sdk):
-    mock_globus_sdk.id_token = {'preferred_username': 'wash@serenity.com'}
+async def test_restricted_domain(globus_client):
     authenticator = GlobusOAuthenticator()
     authenticator.identity_provider = 'alliance.gov'
-    handler = globus_client.handler_for_user(user_model('wash'))
+    handler = globus_client.handler_for_user(user_model('wash@uflightacademy.edu'))
     with raises(web.HTTPError) as exc:
         await authenticator.authenticate(handler)
     assert exc.value.status_code == 403
 
 
-async def test_namespaced_domain(globus_client, mock_globus_sdk):
-    mock_globus_sdk.id_token = {'preferred_username':
-                                'wash@legitshipping.com@serenity.com'}
+async def test_namespaced_domain(globus_client):
     authenticator = GlobusOAuthenticator()
     # Allow any idp
     authenticator.identity_provider = ''
-    handler = globus_client.handler_for_user(user_model('wash'))
+    um = user_model('wash@legitshipping.com@serenity.com')
+    handler = globus_client.handler_for_user(um)
     data = await authenticator.authenticate(handler)
     assert data['name'] == 'wash'
 
 
-async def test_token_exclusion(globus_client, mock_globus_sdk):
+async def test_token_exclusion(globus_client):
     authenticator = GlobusOAuthenticator()
     authenticator.exclude_tokens = [
         'transfer.api.globus.org',
         'auth.globus.org'
     ]
-    handler = globus_client.handler_for_user(user_model('wash'))
+    handler = globus_client.handler_for_user(user_model('wash@uflightacademy.edu'))
     data = await authenticator.authenticate(handler)
     assert data['name'] == 'wash'
     assert list(data['auth_state']['tokens'].keys()) == []
 
 
-def test_revoke_tokens(monkeypatch):
-    monkeypatch.setattr(
-        ConfidentialAppAuthClient,
-        'oauth2_revoke_token',
-        Mock()
-    )
+async def test_revoke_tokens(globus_client, mock_globus_user):
+    globus_client.add_host('auth.globus.org', [('/v2/oauth2/token/revoke',
+                                               revoke_token_request_handler)])
     authenticator = GlobusOAuthenticator()
     service = {'transfer.api.globus.org': {'access_token': 'foo',
                                            'refresh_token': 'bar'}}
-    authenticator.revoke_service_tokens(service)
-    assert ConfidentialAppAuthClient.oauth2_revoke_token.called
+    authenticator.current_user = mock_globus_user
+    await authenticator.revoke_service_tokens(service)
 
 
 async def test_custom_logout(monkeypatch, mock_globus_user):
@@ -155,11 +203,7 @@ async def test_custom_logout(monkeypatch, mock_globus_user):
     authenticator = GlobusOAuthenticator()
     logout_handler = mock_handler(GlobusLogoutHandler,
                                   authenticator=authenticator)
-    monkeypatch.setattr(
-        web.RequestHandler,
-        'redirect',
-        Mock()
-    )
+    monkeypatch.setattr(web.RequestHandler, 'redirect', Mock())
     logout_handler.clear_login_cookie = Mock()
     logout_handler.get_current_user = Mock(return_value=mock_globus_user)
     logout_handler._jupyterhub_user = mock_globus_user
@@ -177,26 +221,22 @@ async def test_custom_logout(monkeypatch, mock_globus_user):
     assert logout_handler.clear_login_cookie.called
 
 
-async def test_logout_revokes_tokens(monkeypatch, mock_globus_user):
-
+async def test_logout_revokes_tokens(globus_client, monkeypatch, mock_globus_user):
+    globus_client.add_host('auth.globus.org', [('/v2/oauth2/token/revoke',
+                                               revoke_token_request_handler)])
     authenticator = GlobusOAuthenticator()
-    logout_handler = mock_handler(GlobusLogoutHandler,
-                                  authenticator=authenticator)
-    monkeypatch.setattr(
-        web.RequestHandler,
-        'redirect',
-        Mock()
-    )
+    logout_handler = mock_handler(GlobusLogoutHandler, authenticator=authenticator)
+
+    # Setup
+    monkeypatch.setattr(web.RequestHandler, 'redirect', Mock())
     logout_handler.get_current_user = Mock(return_value=mock_globus_user)
     logout_handler._jupyterhub_user = mock_globus_user
     monkeypatch.setitem(logout_handler.settings, 'statsd', Mock())
     monkeypatch.setitem(logout_handler.settings, 'login_url', '')
 
     logout_handler.clear_login_cookie = Mock()
-    authenticator.revoke_service_tokens = Mock()
     authenticator.revoke_tokens_on_logout = True
 
     await logout_handler.get()
-    assert authenticator.revoke_service_tokens.called
     auth_state = await mock_globus_user.get_auth_state()
-    assert auth_state == {'tokens': ''}
+    assert auth_state == {'tokens': {}}
